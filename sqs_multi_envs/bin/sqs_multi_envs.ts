e8f92e2900e7b7358b5e190a34a3f376d662a48a
zakzakst <e1433104@gmail.com>
2026-01-20 19:34:23 +0900

    section5

--- sqs_multi_envs/bin/sqs_multi_envs.ts
+++ sqs_multi_envs/bin/sqs_multi_envs.ts
@@ -14,17 +14,31 @@ if (!environment) {
   throw new Error(`Invalid stage: ${stage}`);
 }
 
-const env: cdk.Environment = {
-  account: environment.account,
-  region: environment.region,
-}
+// const env: cdk.Environment = {
+//   account: environment.account,
+//   region: environment.region,
+// }
 
 const app = new cdk.App();
-new SqsMultiEnvsStack(app, `${stage}-SqsMultiEnvsStack1`, {
-  queueName: `${stage}-MyQueue1`,
-  env,
-});
-new SqsMultiEnvsStack(app, `${stage}-SqsMultiEnvsStack2`, {
-  queueName: `${stage}-MyQueue2`,
-  env,
-});
+// new SqsMultiEnvsStack(app, `${stage}-SqsMultiEnvsStack1`, {
+//   queueName: `${stage}-MyQueue1`,
+//   env,
+// });
+// new SqsMultiEnvsStack(app, `${stage}-SqsMultiEnvsStack2`, {
+//   queueName: `${stage}-MyQueue2`,
+//   env,
+// });
+
+const st = new cdk.Stage(app, stage, {
+  env: {
+    account: environment.account,
+    region: environment.region,
+  }
+})
+
+new SqsMultiEnvsStack(st, 'SqsMultiStack1', {
+  queueName: `${stage}-MyQueue1`
+})
+new SqsMultiEnvsStack(st, 'SqsMultiStack2', {
+  queueName: `${stage}-MyQueue2`
+})
