f823fb4596bcd1df60dc68131905fb2a98bee1c3
zakzakst <e1433104@gmail.com>
2026-01-22 09:13:32 +0900

    section5

--- sqs_test/test/sqs_test.test.ts
+++ sqs_test/test/sqs_test.test.ts
@@ -1,13 +1,37 @@
 import * as cdk from 'aws-cdk-lib/core';
 import { Template } from 'aws-cdk-lib/assertions';
 import * as SqsTest from '../lib/sqs_test-stack';
-import { props } from '../lib/config';
+import { propsLong, propsShort, STAGE } from '../lib/config';
+import 'jest-specific-snapshot';
+import * as path from 'path';
 
-describe('SQS Stack', () => {
+const stages = Object.keys(propsLong) as STAGE[];
+
+const snapshotFileName = (stage: string, stackName: string): string => {
+    return `./__snapshots__/${path.basename(__filename).split('.')[0]}/${stage}/${stackName}.snapshot`
+}
+
+describe.each(stages)('[stage: %s] should match snapshot', (stage) => {
     const app = new cdk.App();
-    const stack = new SqsTest.SqsTestStack(app, 'SqsTestStack', props);
-    const template = Template.fromStack(stack).toJSON();
-    it('should match the snapshot', () => {
-        expect(template).toMatchSnapshot();
-    });
-});
+
+    const stacks = [
+        new SqsTest.SqsTestStack(app, 'SqsTestLongStack', propsLong[stage]),
+        new SqsTest.SqsTestStack(app, 'SqsTestShortStack', propsShort[stage]),
+    ]
+
+    stacks.forEach((stack) => {
+        it(`[${stack.stackName}] should match the snapshot`, () => {
+            const template = Template.fromStack(stack).toJSON();
+            expect(template).toMatchInlineSnapshot(snapshotFileName(stage, stack.stackName))
+        })
+    })
+})
+
+// describe('SQS Stack', () => {
+//     const app = new cdk.App();
+//     const stack = new SqsTest.SqsTestStack(app, 'SqsTestStack', props);
+//     const template = Template.fromStack(stack).toJSON();
+//     it('should match the snapshot', () => {
+//         expect(template).toMatchSnapshot();
+//     });
+// });
