b1ada186ffb51bf8a088656feeb35fea2dfb7506
zakzakst <e1433104@gmail.com>
2026-02-01 20:40:26 +0900

    fix

---
+++ lambda_print_event_py/test/lambda_print_event_py.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as LambdaPrintEventPy from '../lib/lambda_print_event_py-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/lambda_print_event_py-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new LambdaPrintEventPy.LambdaPrintEventPyStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
